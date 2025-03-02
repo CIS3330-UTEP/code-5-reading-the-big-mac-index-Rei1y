7c035b15a70249e9274bfa3ceba8d3a2dc752a13
Rei1y <reiyrasec05@gmail.com>
2025-03-02 15:09:13 -0700

    try 7

--- code_4.py
+++ code_4.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-# big_mac_file = './big-mac-full-index.csv'
-
-
 df = pd.read_csv('./big-mac-full-index.csv')
 
 
@@ -14,7 +11,6 @@ def get_big_mac_price_by_year(year, country_code):
             total_price += row['dollar_price']
             count += 1
     if count > 0:
-        # return round(total_price / count, 2)
         return f"${round(total_price / count, 2)}"
     else:
         return None
@@ -29,33 +25,32 @@ def get_big_mac_price_by_country(country_code):
             count += 1
     if count > 0:
         return f"${round(total_price / count, 2)}"
-    # return f"${round(total_price / count, 2)}"
     else:
         return None
 
 
 def get_the_cheapest_big_mac_price_by_year(year):
     minimum_price = float('inf')
-    min_country = None
+    minimum_country = None
     for _, row in df.iterrows():
         if row['date'].startswith(str(year)) and row['dollar_price'] < minimum_price:
             minimum_price = row['dollar_price']
-            min_country = row
-    if min_country is not None:
-        return f"{min_country['name']}({min_country['iso_a3']}): ${round(minimum_price, 2)}"
+            minimum_country = row
+    if minimum_country is not None:
+        return f"{minimum_country['name']}({minimum_country['iso_a3']}): ${round(minimum_price, 2)}"
     else:
         return None
 
 
 def get_the_most_expensive_big_mac_price_by_year(year):
     maximum_price = float('-inf')
-    max_country = None
+    maximum_country = None
     for _, row in df.iterrows():
         if row['date'].startswith(str(year)) and row['dollar_price'] > maximum_price:
             maximum_price = row['dollar_price']
-            max_country = row
-    if max_country is not None:
-        return f"{max_country['name']}({max_country['iso_a3']}): ${round(maximum_price, 2)}"
+            maximum_country = row
+    if maximum_country is not None:
+        return f"{maximum_country['name']}({maximum_country['iso_a3']}): ${round(maximum_price, 2)}"
     else:
         return None
 
